6eb52d6d870c7ed71f8c2900f898dbff0189bdbe
seemapoudel58 <simapoudel6@gmail.com>
2024-03-02 21:52:13 +0545

    add virtual method

---
+++ Assignment2/OnlineBookStore/Book.cs
@@ -0,0 +1,29 @@
+public class Book : StoreItem, IDiscountable
+{
+    public string Title { get; set; }
+    private decimal discountRate;
+    public Book (string title, decimal price, decimal discountRate) : base(price)
+    {
+        Title = title;
+        this.discountRate = discountRate;
+    }
+
+    // Implementing the abstract DisplayInfo method
+     public override string DisplayInfo()
+     {
+        return $"Title: {Title}, Original Price: ${OriginalPrice}";
+     }
+
+     //Implementing GetDiscountRate from both StoreItem and IDiscountable
+
+     public override decimal GetDiscountRate()
+     {
+        return discountRate;
+     }
+
+    //New virtual method to demonstrate overriding
+     public virtual string GetBookCategory()
+    {
+        return "General";
+    }
+}
