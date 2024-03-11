09f343e550988ab403dae4944ec7589b67fbee84
seemapoudel58 <simapoudel6@gmail.com>
2024-03-11 09:26:19 +0545

    Create Inflation class

---
+++ Assignment3/Inflation/Inflation.cs
@@ -0,0 +1,11 @@
+public class Inflation 
+
+{
+    public string RegionMember { get; set; }
+    public int Year { get; set; }
+    public double Inflation { get; set; }
+    public string UnitOfMeasurement { get; set; }
+    public string Subregion { get; set; }
+    public string CountryCode { get; set; }
+
+}
