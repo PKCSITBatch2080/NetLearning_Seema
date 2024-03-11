7e6c3c45530b89a7554d5d2d95415695fa8259b7
seemapoudel58 <simapoudel6@gmail.com>
2024-03-11 12:47:03 +0545

    complete assignment 3

--- Assignment3/Inflation/Inflation.cs
+++ Assignment3/Inflation/Inflation.cs
@@ -1,11 +1,11 @@
 public class Inflation 
 
 {
-    public string RegionMember { get; set; }
+    public string RegionalMember { get; set; }= string.Empty;
     public int Year { get; set; }
-    public double Inflation { get; set; }
-    public string UnitOfMeasurement { get; set; }
-    public string Subregion { get; set; }
-    public string CountryCode { get; set; }
+    public double InflationRate { get; set; }
+    public string? UnitOfMeasurement { get; set; }
+    public string? Subregion { get; set; }= string.Empty;
+    public string? CountryCode { get; set; }= string.Empty;
 
 }
